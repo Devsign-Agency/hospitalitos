957e3e0fa9332a21175d3be4beb6814e2f4cd829
bpena <bernardo.pena.ramos@gmail.com>
2023-01-16 18:23:02 -0400

    adding winston logger

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -6,11 +6,35 @@
 import { Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { WinstonModule } from 'nest-winston';
+import * as winston from 'winston';
 
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
+    const app = await NestFactory.create(AppModule, {
+        logger: WinstonModule.createLogger({
+            // options (same as WinstonModule.forRoot() options)
+            level: 'info',
+            format: winston.format.combine(
+                winston.format.colorize(),
+                winston.format.timestamp(),
+                winston.format.ms(),
+                winston.format.printf((info) => {
+                    const { timestamp, level, message, ...args } = info;
+
+                    const ts = timestamp.slice(0, 19).replace('T', ' ');
+                    return `${ts} [${level}]: ${args.context ? ('[' + args.context + ']') : ''} ${message} `;
+                }),
+            ),
+            defaultMeta: { service: 'api' },
+            transports: [
+                new winston.transports.File({ dirname: 'logs', filename: 'error.log', level: 'error' }),
+                new winston.transports.File({ dirname: 'logs', filename: 'api.log' }),
+                new winston.transports.Console()
+            ]
+        })
+    });
     const config = app.get(ConfigService);
     const globalPrefix = `${config.get('globalPrefix')}`;
     app.setGlobalPrefix(globalPrefix);
