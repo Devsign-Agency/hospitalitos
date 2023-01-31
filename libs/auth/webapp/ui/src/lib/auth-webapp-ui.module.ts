824139a73808b67a5997b33db4d4eba71cf91410
bpena <bernardo.pena.ramos@gmail.com>
2023-01-31 12:20:53 -0400

    adding login and register page

--- libs/auth/webapp/ui/src/lib/auth-webapp-ui.module.ts
+++ libs/auth/webapp/ui/src/lib/auth-webapp-ui.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LoginComponent } from './views/login/login.component';
+import { RegisterComponent } from './views/register/register.component';
 
 @NgModule({
     imports: [CommonModule],
+    declarations: [LoginComponent, RegisterComponent],
 })
 export class AuthWebappUiModule {}
