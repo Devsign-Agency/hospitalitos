10b4325186f621cd721753b26ff25b024a8916bc
bpena <bernardo.pena.ramos@gmail.com>
2023-02-06 12:26:37 -0400

    adding match validator

--- libs/security/webapp/ui/src/lib/user/views/form/form.component.ts
+++ libs/security/webapp/ui/src/lib/user/views/form/form.component.ts
@@ -1,8 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { UserService } from '@kaad/security/webapp/core';
+import { matchValidator } from '@kaad/shared/webapp/ui';
 
 @Component({
     selector: 'kaad-form',
     templateUrl: './form.component.html',
     styleUrls: ['./form.component.scss'],
 })
-export class FormComponent {}
+export class FormComponent implements OnInit {
+
+    form: FormGroup;
+    title = '';
+
+    constructor(formBuilder: FormBuilder,
+                private readonly route: ActivatedRoute,
+                private readonly userService: UserService) {
+        this.form = formBuilder.group({
+            id: [null],
+            firstname: [''],
+            lastname: [''],
+            email: ['', [Validators.required, Validators.email]],
+            username: ['', Validators.required],
+            photoUrl: [''],
+            newpassword: ['', [
+                // Validators.required,
+                // Validators.pattern('^(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9]+)$'),
+                // Validators.minLength(6),
+                // Validators.maxLength(25),
+                matchValidator('confirmPassword', true)
+              ]],
+              confirmPassword: ['', [
+                // Validators.required,
+                matchValidator('newpassword')
+              ]]
+        });
+    }
+
+    ngOnInit(): void {
+        const id = this.route.snapshot.params['id'];
+        this.title = id  ? 'Edit User' : 'New User';
+        if (id) {
+            this.form.controls['email'].disable();
+            this.userService.findById(id).subscribe({
+                next: user => this.form.patchValue(user)
+            });
+        }
+    }
+
+    save() {
+        if (this.form.valid) {
+            //
+        }
+    }
+}
--- libs/shared/webapp/ui/src/index.ts
+++ libs/shared/webapp/ui/src/index.ts
@@ -1,2 +1,3 @@
 export * from './lib/shared-webapp-ui.module';
 export * from './lib/resolvers/data.resolver';
+export * from './lib/validators/match.validator';
