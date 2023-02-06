028aa6bef4cf5aab87b9ac3b1c030a71f891d01d
bpena <bernardo.pena.ramos@gmail.com>
2023-02-05 21:27:09 -0400

    adding detail view

--- libs/security/webapp/ui/src/lib/user/views/detail/detail.component.ts
+++ libs/security/webapp/ui/src/lib/user/views/detail/detail.component.ts
@@ -1,8 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { User } from '@kaad/security/ng-common';
+import { UserService } from '@kaad/security/webapp/core';
+import { Observable } from 'rxjs';
 
 @Component({
     selector: 'kaad-detail',
     templateUrl: './detail.component.html',
     styleUrls: ['./detail.component.scss'],
 })
-export class DetailComponent {}
+export class DetailComponent implements OnInit {
+
+    user$?: Observable<User>;
+
+    constructor(private readonly route: ActivatedRoute,
+                private readonly userService: UserService) { }
+
+    ngOnInit(): void {
+        const id = this.route.snapshot.params['id'];
+        if (id) {
+            this.findUser(id);
+        }
+    }
+
+    findUser(id: string) {
+        this.user$ = this.userService.findById(id);
+    }
+}
