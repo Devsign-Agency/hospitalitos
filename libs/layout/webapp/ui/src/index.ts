b56ea16264fbdc0968e68fab5938676fbbfe3e0e
bpena <bernardo.pena.ramos@gmail.com>
2023-02-15 06:03:31 -0400

    export libraries from layout

--- libs/layout/webapp/ui/src/index.ts
+++ libs/layout/webapp/ui/src/index.ts
@@ -5,8 +5,24 @@ export * from './lib/directives/content.directive';
 export * from './lib/services/aside.service';
 export * from './lib/services/modal.service';
 export * from './lib/services/toast.service';
+export * from './lib/services/loading.service';
 
 export * from './lib/types/data-table.type';
 
 export * from './lib/components/modal/modal.component';
-export * from './lib/services/loading.service';
+export * from './lib/components/aside/aside.component';
+export * from './lib/components/breadcrumb/breadcrumb.component';
+export * from './lib/components/footer/footer.component';
+export * from './lib/components/header/header.component';
+export * from './lib/components/loading/loading.component';
+export * from './lib/components/menu/menu.component';
+export * from './lib/components/pagination/pagination.component';
+export * from './lib/components/toast/toast.component';
+
+export * from './lib/layouts/main/main.component';
+export * from './lib/layouts/security/security.component';
+
+export * from './lib/views/detail/detail.view';
+export * from './lib/views/form/form.view';
+export * from './lib/views/list/list.view';
+
