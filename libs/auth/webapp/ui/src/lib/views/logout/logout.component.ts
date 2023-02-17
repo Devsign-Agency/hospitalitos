293a2461b16338676cbd8e5ad07e354a4d2f8ef7
bpena <bernardo.pena.ramos@gmail.com>
2023-02-17 00:44:55 -0400

    adding template for google login callback page and logout page

--- libs/auth/webapp/ui/src/lib/views/logout/logout.component.ts
+++ libs/auth/webapp/ui/src/lib/views/logout/logout.component.ts
@@ -3,7 +3,14 @@ import { AuthService } from '@kaad/auth/webapp/core';
 
 @Component({
     selector: 'kaad-logout',
-    template: ''
+    template: `
+    <div class="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-white">
+        <div class="flex-auto px-4 py-10">
+            <h1 class="text-2xl">Logout.</h1>
+            <span>please await while we close session...</span>
+        </div>
+    </div>
+    `
 })
 export class LogoutComponent implements OnInit {
     constructor(private readonly authService: AuthService) {}
