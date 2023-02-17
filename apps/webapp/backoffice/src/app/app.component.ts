d248d19f3ddcec21a7ef81b7dec504cd7bb00633
bpena <bernardo.pena.ramos@gmail.com>
2023-02-16 23:07:33 -0400

    agregando el logo y el titulo de la pagina

--- apps/webapp/backoffice/src/app/app.component.ts
+++ apps/webapp/backoffice/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
     styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-    title = 'webapp-backoffice';
+    title = 'Hospitalitos de la Fe <<Backoffice>>';
 }
