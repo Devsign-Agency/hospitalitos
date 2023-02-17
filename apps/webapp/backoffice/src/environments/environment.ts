50289e6918ec7fd7874a493da2bc552b30a4c8b6
bpena <bernardo.pena.ramos@gmail.com>
2023-02-16 23:11:28 -0400

    setting urlFront on environments

--- apps/webapp/backoffice/src/environments/environment.ts
+++ apps/webapp/backoffice/src/environments/environment.ts
@@ -8,6 +8,6 @@ export const environment = {
         loginPage: 'auth/login',
         logoutPage: 'auth/logout',
         homePage: '/',
-        urlFront: 'http://localhost:4200'
+        urlFront: 'http://backoffice.hospitalitosdelafe.org'
     },
 };
