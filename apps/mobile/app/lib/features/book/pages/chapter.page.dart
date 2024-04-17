14c8f52fe22ac32817284d7d39096cf0455d94cf
rquero <ricardoequero@gmail.com>
2024-04-17 14:16:20 -0400

    fix: resolve conflict in chapter page

--- apps/mobile/app/lib/features/book/pages/chapter.page.dart
+++ apps/mobile/app/lib/features/book/pages/chapter.page.dart
@@ -75,20 +75,6 @@ class _ChapterPageState extends State<ChapterPage> {
   bool isCurrentLanguageInstalled = false;
   int end = 0;
   int positionLastWord = 0;
-<<<<<<< HEAD
-  late PdfViewerController _pdfViewerController;
-  OverlayEntry? _overlayEntry;
-  int i = 0;
-  String? _newVoiceText;
-  List<String> sentences = [];
-  TtsState ttsState = TtsState.stopped;
-  int _newVoiceTextLenght = 0;
-  String parsedEpubString = '';
-  get isPlaying => ttsState == TtsState.playing;
-  get isStopped => ttsState == TtsState.stopped;
-  get isPaused => ttsState == TtsState.paused;
-  get isContinued => ttsState == TtsState.continued;
-=======
 
   TextBook textBook = TextBook(
       fontFamily: 'fontFamily',
@@ -109,7 +95,6 @@ class _ChapterPageState extends State<ChapterPage> {
   double offsetScroll = 0;
   ScrollController scrollController =
       ScrollController(initialScrollOffset: 0.0);
->>>>>>> feat/subchapters
 
   int bottomNavigationBarCurrentIndex = 0;
 
@@ -123,170 +108,19 @@ class _ChapterPageState extends State<ChapterPage> {
   @override
   void initState() {
     super.initState();
-<<<<<<< HEAD
-    initTts();
-    getTextFromEpubInstance();
-=======
->>>>>>> feat/subchapters
 
     fToast = FToast();
     fToast?.init(context);
   }
 
-<<<<<<< HEAD
-  buildAudioText(String newVoiceText) async {
-    var count = newVoiceText.length;
-    var max = 4000;
-    var loopCount = count ~/ max;
-
-    for (var i = 0; i <= loopCount; i++) {
-      if (i != loopCount) {
-        sentences.add(newVoiceText.substring(i * max, (i + 1) * max));
-        // await flutterTts
-        //     .speak(_newVoiceText!.substring(i * max, (i + 1) * max));
-      } else {
-        var end = (count - ((i * max)) + (i * max));
-        sentences.add(newVoiceText.substring(i * max, end));
-
-        // await flutterTts.speak(_newVoiceText!.substring(i * max, end));
-      }
-    }
-    _newVoiceTextLenght = loopCount;
-    _newVoiceText = sentences[i];
-    i++;
-    setState(() {});
-  }
-
-  initTts() {
-    flutterTts = FlutterTts();
-=======
   @override
   void didChangeDependencies() {
     super.didChangeDependencies();
->>>>>>> feat/subchapters
 
     final arguments =
         ModalRoute.of(context)!.settings.arguments as EpubArguments;
 
-<<<<<<< HEAD
-    if (isAndroid) {
-      _getEngines();
-    }
-
-    flutterTts.setStartHandler(() {
-      setState(() {
-        print("Playing");
-        ttsState = TtsState.playing;
-      });
-    });
-
-    flutterTts.setCompletionHandler(() {
-      setState(() {
-        print("Complete");
-        ttsState = TtsState.stopped;
-        i++;
-        if (i < _newVoiceTextLenght) {
-          _newVoiceText = sentences[i];
-          _speak();
-        } else {
-          positionLastWord = 0;
-        }
-      });
-    });
-
-    flutterTts.setCancelHandler(() {
-      setState(() {
-        print("Cancel");
-        ttsState = TtsState.stopped;
-      });
-    });
-
-    if (isWeb || isIOS) {
-      flutterTts.setPauseHandler(() {
-        setState(() {
-          print("Paused");
-          ttsState = TtsState.paused;
-        });
-      });
-
-      flutterTts.setContinueHandler(() {
-        setState(() {
-          print("Continued");
-          ttsState = TtsState.continued;
-        });
-      });
-    }
-
-    flutterTts.setErrorHandler((msg) {
-      setState(() {
-        print("error: $msg");
-        ttsState = TtsState.stopped;
-      });
-    });
-
-    flutterTts.setProgressHandler(
-        (String text, int startOffset, int endOffset, String word) {
-      print('text: $text');
-      print('startOffset: $startOffset');
-      print('endOffset: $endOffset');
-      print('word: $word');
-      setState(() {
-        // int index = _newVoiceText!.indexOf(word);
-
-        // end = index + word.length;
-
-        end = endOffset + positionLastWord;
-      });
-    });
-  }
-
-  Future _getLanguages() async {
-    languages = await flutterTts.getLanguages;
-    if (languages != null) setState(() => languages);
-  }
-
-  Future _getEngines() async {
-    var engines = await flutterTts.getEngines;
-    if (engines != null) {
-      for (dynamic engine in engines) {
-        print(engine);
-      }
-    }
-  }
-
-  Future _speak() async {
-    await flutterTts.setVolume(volume);
-    await flutterTts.setSpeechRate(rate);
-    await flutterTts.setPitch(pitch);
-    ttsState = TtsState.playing;
-    // if (newVoiceText) {
-    await flutterTts.awaitSpeakCompletion(true);
-    var result = await flutterTts.speak(_newVoiceText!);
-    print("RESULT $result");
-    if (result == 1) setState(() => ttsState = TtsState.playing);
-    // }
-  }
-
-  Future _stop() async {
-    var result = await flutterTts.stop();
-    if (result == 1) setState(() => ttsState = TtsState.stopped);
-  }
-
-  Future _pause() async {
-    positionLastWord = end;
-
-    var result = await flutterTts.pause();
-    if (result == 1) setState(() => ttsState = TtsState.paused);
-  }
-
-  _playText(parsedString) async {
-    print('playText');
-    TextToSpeech tts = TextToSpeech();
-
-    onAudioSound ? tts.play(parsedString) : tts.stop();
-=======
     parsedString = _parseDocumentToString(arguments.chapter!);
->>>>>>> feat/subchapters
   }
 
   @override
@@ -309,43 +143,7 @@ class _ChapterPageState extends State<ChapterPage> {
 
     data != null ? _onChange(data.text!) : _onChange('');
 
-<<<<<<< HEAD
-    // //Extract all the text from the document.
-    // String text = extractor.extractText();
-
-    //Display the text.
-    // _showResult(text);
-    // print(text);
-
-    //Load the PDF document.
-    // PdfDocument loadedDocument = PdfDocument(
-    //     inputBytes:
-    //         File('assets/pdf/G.A.E - 19 nov 2022.pdf').readAsBytesSync());
-
-    // //Get the first page from the document.
-    // PdfPage loadedPage = loadedDocument.pages[0];
-    // //Create a PDF Template.
-    // PdfTemplate template = loadedPage.createTemplate();
-    // //Create a new PDF document.
-    // PdfDocument document = PdfDocument();
-    // //Add the page.
-    // PdfPage page = document.pages.add();
-    // //Create the graphics.
-    // PdfGraphics graphics = page.graphics;
-    // //Draw the template.
-    // graphics.drawPdfTemplate(template, Offset(0, 0));
-    // //Save and dispose of the PDF document.
-    // File('Output.pdf').writeAsBytes(await document.save());
-    // document.dispose();
-
-    // ClipboardData? data = await Clipboard.getData(Clipboard.kTextPlain);
-
-    // _onChange(data!.text!);
-
-    setState(() {});
-=======
     // _speak();
->>>>>>> feat/subchapters
   }
 
   void _onChange(String text) {
@@ -354,66 +152,6 @@ class _ChapterPageState extends State<ChapterPage> {
     });
   }
 
-<<<<<<< HEAD
-  getTextFromEpubInstance() async {
-    final byteData = await rootBundle.load('assets/epubs/book.epub');
-    Directory tempDir = await getTemporaryDirectory();
-
-    File tempVideo = File("${tempDir.path}/assets/my_video.mp4")
-      ..createSync(recursive: true)
-      ..writeAsBytesSync(byteData.buffer
-          .asUint8List(byteData.offsetInBytes, byteData.lengthInBytes));
-
-    File _epubFile = File(tempVideo.path);
-    final contents = await _epubFile.readAsBytes();
-    EpubBookRef epub = await EpubReader.openBook(contents.toList());
-    var cont = await EpubReader.readTextContentFiles(epub.Content!.Html!);
-    List<String> htmlList = [];
-    for (var value in cont.values) {
-      htmlList.add(value.Content!);
-    }
-    var doc = parse(htmlList.join());
-    final String parsedString = parse(doc.body!.text).documentElement!.text;
-
-    // print('text: $parsedString');
-    parsedEpubString = parsedString;
-    print(parsedEpubString);
-    buildAudioText(parsedString);
-
-    // setState(() {
-    //   _newVoiceText = parsedString;
-
-    //   print(_newVoiceText);
-    // });
-    // // await _playText(parsedString);
-  }
-
-  void _showContextMenu(
-      BuildContext context, PdfTextSelectionChangedDetails details) {
-    final OverlayState overlayState = Overlay.of(context);
-    _overlayEntry = OverlayEntry(
-      builder: (context) => Positioned(
-        top: details.globalSelectedRegion!.center.dy - 55,
-        left: details.globalSelectedRegion!.bottomLeft.dx,
-        child: ElevatedButton(
-          onPressed: () {
-            if (details.selectedText != null) {
-              Clipboard.setData(ClipboardData(text: details.selectedText!));
-              print('Text copied to clipboard: ${details.selectedText}');
-              _pdfViewerController.clearSelection();
-            }
-          },
-          style: ButtonStyle(
-            shape: MaterialStateProperty.all(RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(2),
-            )),
-          ),
-          child: const Text('Copy', style: TextStyle(fontSize: 17)),
-        ),
-      ),
-    );
-    overlayState.insert(_overlayEntry!);
-=======
   void setScrollController(double offset) async {
     scrollController.removeListener(() {});
     scrollController = ScrollController(initialScrollOffset: offset);
@@ -426,7 +164,6 @@ class _ChapterPageState extends State<ChapterPage> {
       offsetScroll = scrollController.position.pixels;
     });
     setState(() {});
->>>>>>> feat/subchapters
   }
 
   void _handleTapPageViewMarkerList(double offset) {
@@ -487,163 +224,6 @@ class _ChapterPageState extends State<ChapterPage> {
     setState(() {});
   }
 
-<<<<<<< HEAD
-  Widget _btnSection() {
-    print('TtsState: $ttsState');
-    if (isAndroid) {
-      if (ttsState != TtsState.playing) {
-        print('Not playing: $ttsState');
-        return _buildButtonColumn(Colors.red, Colors.redAccent,
-            ImageConstant.imgArrowMedia, '', _speak);
-      } else {
-        print('Playing: $ttsState');
-        return _buildButtonColumn(Colors.red, Colors.redAccent,
-            ImageConstant.imgArrowdown, '', _pause);
-      }
-    }
-
-    return Container();
-  }
-
-  CustomIconButton _buildButtonColumn(Color color, Color splashColor,
-      String icon, String label, Function func) {
-    return CustomIconButton(
-      margin: getMargin(left: 8),
-      height: getSize(58),
-      width: getSize(58),
-      variant: IconButtonVariant.FillIndigo,
-      onTap: () => func(),
-      child: CustomImageView(
-        svgPath: icon,
-      ),
-    );
-  }
-
-  Widget _progressBar(int end) {
-    // print('newVoiceText: $_newVoiceText $');
-    print('value ${end / parsedEpubString.length}');
-    return Container(
-        alignment: Alignment.topCenter,
-        padding: EdgeInsets.only(top: 5.0, right: 10),
-        child: LinearProgressIndicator(
-          backgroundColor: ColorConstant.indigo90033,
-          valueColor: AlwaysStoppedAnimation<Color>(ColorConstant.indigo900),
-          value: end / parsedEpubString.length,
-        ));
-  }
-
-  void _handleChangeIndex(int index) {
-    switch (index) {
-      case 2:
-        _pdfViewerKey.currentState?.openBookmarkView();
-
-        break;
-    }
-  }
-
-  Widget _buildSliders() {
-    return Column(
-      children: [_volume(), _pitch(), _rate()],
-    );
-  }
-
-  Widget _volume() {
-    return Slider(
-        value: volume,
-        onChanged: (newVolume) {
-          setState(() => volume = newVolume);
-        },
-        min: 0.0,
-        max: 1.0,
-        divisions: 10,
-        label: "Volume: $volume");
-  }
-
-  Widget _pitch() {
-    return Slider(
-      value: pitch,
-      onChanged: (newPitch) {
-        setState(() => pitch = newPitch);
-      },
-      min: 0.5,
-      max: 2.0,
-      divisions: 15,
-      label: "Pitch: $pitch",
-      activeColor: Colors.red,
-    );
-  }
-
-  Widget _rate() {
-    return Slider(
-      value: rate,
-      onChanged: (newRate) {
-        setState(() => rate = newRate);
-      },
-      min: 0.0,
-      max: 1.0,
-      divisions: 10,
-      label: "Rate: $rate",
-      activeColor: Colors.green,
-    );
-  }
-
-  Future<List<int>> _readDocumentData(String name) async {
-    final ByteData data = await rootBundle.load('assets/pdf/$name');
-    return data.buffer.asUint8List(data.offsetInBytes, data.lengthInBytes);
-  }
-
-  void _showResult(String text) {
-    showDialog(
-        context: context,
-        builder: (BuildContext context) {
-          return AlertDialog(
-            title: Text('Extracted text'),
-            content: Scrollbar(
-              child: SingleChildScrollView(
-                physics: BouncingScrollPhysics(
-                    parent: AlwaysScrollableScrollPhysics()),
-                child: Text(text),
-              ),
-            ),
-            actions: [
-              ElevatedButton(
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
-                  child: Text('Show Text'))
-            ],
-          );
-        });
-  }
-}
-
-class _CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
-  final Color? backgroundColor;
-  final Color? textIconColor;
-  final String? icon;
-  final String? title;
-  final double? height;
-  final List<Widget>? menuItem;
-  final bool hideBack;
-  final bool audioActive;
-  final Function onChangeStatusAudio;
-
-  const _CustomAppBar({
-    super.key,
-    this.backgroundColor = Colors.red,
-    this.textIconColor = Colors.red,
-    this.icon,
-    this.title = '',
-    this.menuItem,
-    this.height: kToolbarHeight,
-    this.hideBack = false,
-    required this.audioActive,
-    required this.onChangeStatusAudio,
-  });
-  @override
-  Size get preferredSize => Size.fromHeight(height!);
-
-=======
   String _parseDocumentToString(EpubChapter? chapter) {
     dynamic htmlContent = parse(chapter!.HtmlContent!);
 
@@ -674,7 +254,6 @@ class _CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
     return color;
   }
 
->>>>>>> feat/subchapters
   @override
   Widget build(BuildContext context) {
     final arguments =
