a0ec0b50b01f733a379518e4886ddc3486e5fdb3
rquero <ricardoequero@gmail.com>
2024-04-30 13:55:27 -0400

    feat: integration of readings and prayers

---
+++ apps/mobile/app/lib/features/prayer/screens/prayer_detail/prayer_detail_screen.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:mobile_app/core/app_export.dart';
+
+import '../../../../widgets/widgets.dart';
+
+class PrayerDetailScreen extends StatelessWidget {
+  static const String route = 'prayer-detail-router';
+  const PrayerDetailScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Prayer prayer = ModalRoute.of(context)?.settings.arguments! as Prayer;
+
+    return Scaffold(
+      appBar: CustomAppBar(title: prayer.title),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Center(
+              child: Text(
+                prayer.prayer,
+                style: AppStyle.txtNunitoSansSemiBold20Indigo900,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
